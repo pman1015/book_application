0502091cd095c8ec66ea18e98f75157b7fb5e64d
pman1015 <hurd1999@gmail.com>
2024-03-11 21:07:43 -0400

    initial push

---
+++ src/main/java/org/customer_book/PrimaryController.java
@@ -0,0 +1,13 @@
+package org.customer_book;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+        
+    }
+}
---
+++ src/main/java/org/customer_book/SecondaryController.java
@@ -0,0 +1,12 @@
+package org.customer_book;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
